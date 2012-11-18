7525d23e5b1d1453b0343a2dd858587476a29586
Antonio Castro <antonio.alfredo.castro@gmail.com>
2012-11-18 10:17:34 -0500

    Better Storage of individual tweet feed's accuracy

--- trainingError.m
+++ trainingError.m
@@ -10,6 +10,8 @@ correctGuesses_1 = 0;
 correctGuesses_10 = 0;
 totalPopulation = 0;
 
+accuracy = zeros(size(mapF,1),4);
+
 % calculate training error
 for i=1:size(mapF{1,1})
   rawname=mapF{1,2}{i,1};
@@ -18,17 +20,22 @@ for i=1:size(mapF{1,1})
   M = csvread(filename, 1, 0);
   for j=1:size(M,1)
       prediction = nn_predict(filename,1,j);
+           
       % if the correct answer is in the top 5
+      if prediction(1)==mapF{1,1}(i)==1
+          correctGuesses_1 = correctGuesses_1 +1;
+          accuracy(i,1) = accuracy(i,1) + 1;
+      end
       if sum(prediction(1:5,1)==mapF{1,1}(i))==1
           correctGuesses_5 = correctGuesses_5 +1;
+          accuracy(i,2) = accuracy(i,2) + 1;
       end
       if sum(prediction(1:10,1)==mapF{1,1}(i))==1
           correctGuesses_10 = correctGuesses_10 +1;
+          accuracy(i,3) = accuracy(i,3) + 1;
       end
-      if prediction(1)==mapF{1,1}(i)==1
-          correctGuesses_1 = correctGuesses_1 +1;
-      end
-      totalPopulation=totalPopulation+1;          
+      totalPopulation=totalPopulation+1;     
+      accuracy(i,4) = accuracy(1,4)+1;
   end
   mapF{1,2}{i,1}
   correctGuesses_10/totalPopulation  
